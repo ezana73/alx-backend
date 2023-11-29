e556dc5ecf13dcfbaf526bd6e9a225f33848c1da
ezana73 <ezana49@gmail.com>
2023-11-29 15:44:01 +0000

    0-redis_client.js

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,29 +1,14 @@
-import { createClient, print } from 'redis';
+import { createClient } from 'redis';
 
-const client = createClient();
+function redisConnect() {
+  const client = createClient();
 
-client.on('connect', function() {
-  console.log('Redis client connected to the server');
-});
+  client.on('connect', function() {
+    console.log('Redis client connected to the server');
+  }).on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+  });
 
-client.on('error', function (err) {
-  console.log(`Redis client not connected to the server: ${err}`);
-});
-
-function setNewSchool(schoolName, value) {
-  client.set(schoolName, value, print);
 };
 
-function displaySchoolValue(schoolName) {
-  client.get(schoolName, function(error, result) {
-    if (error) {
-      console.log(error);
-      throw error;
-    }
-    console.log(result);
-  });
-}
-
-displaySchoolValue('Holberton');
-setNewSchool('HolbertonSanFrancisco', '100');
-displaySchoolValue('HolbertonSanFrancisco');
+redisConnect();
