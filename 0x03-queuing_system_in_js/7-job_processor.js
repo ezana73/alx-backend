29d6b8d4a1c5968d1772f2e3bc08f841291d92ea
Ezana Zeray <ezana49@gmail.com>
2025-01-08 21:22:16 +0300

    backend

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,16 @@
+import { createQueue } from 'kue';
+
+const blacklist = ['4153518780', '4153518781'];
+
+const queue = createQueue();
+
+function sendNotification(phoneNumber, message, job, done) {
+  job.progress(0, 100);
+  if (blacklist.includes(phoneNumber)) {
+    done(Error(`Phone number ${phoneNumber} is blacklisted`));
+    return;
+  }
+  job.progress(50, 100);
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+  done();
+}
